29bb42b672a481ad348268194c9ba9e3d4fb2d3c
jjortiz <jjortizsanta97@gmail.com>
2025-08-27 23:01:15 -0500

    Fix: Ajuste en implementación de repository

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/UserReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/UserReactiveRepositoryAdapter.java
@@ -1,8 +1,10 @@
 package co.com.bancolombia.r2dbc;
 
 import co.com.bancolombia.model.user.User;
+import co.com.bancolombia.model.user.gateways.UserRepository;
 import co.com.bancolombia.r2dbc.entity.UserEntity;
 import co.com.bancolombia.r2dbc.helper.ReactiveAdapterOperations;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
@@ -10,11 +12,24 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @Repository
-public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<User,UserEntity,Integer,UserReactiveRepository> {
+public class UserReactiveRepositoryAdapter
+        extends ReactiveAdapterOperations<User,UserEntity,Integer,UserReactiveRepository>
+        implements UserRepository{
+
+
     public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, d -> mapper.map(d, User.class));
     }
 
+    @Override
+    public Mono<User> save(User user) {
+        log.debug("MESSAGE_ADAPTER_LOG_TRACE: INIT save user email={}", user.getEmail());
+        return super.save(user)
+                .doOnSuccess(saved -> log.info("MESSAGE_ADAPTER_LOG_TRACE : User saved email={}", saved.getEmail()))
+                .doOnError(err -> log.error("MESSAGE_ADAPTER_LOG_TRACE : DB error while saving user email={} - {}", user.getEmail(), err.getMessage()));
+    }
+
+    @Override
     public Mono<User> findByEmail(String email) {
         return repository.findByEmail(email)
                 .doOnSubscribe(sub -> log.debug("MESSAGE_ADAPTER_LOG_TRACE : INIT findByEmail - email={}", email))
